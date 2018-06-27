35987b0c89c9a8e23fda9138d0c08f29a104a4fa
Joseph <amosjoseph6@gmail.com>
2018-06-27 14:57:14 +0100

    Make changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,7 @@
 // register service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('scripts/sw.js').then(function (registration) {
+        navigator.serviceWorker.register('public/scripts/sw.js').then(function (registration) {
             console.log('Service worker successfully registered on scope', registration.scope);
         }).catch(function (error) {
             console.log('Service worker failed to register');
