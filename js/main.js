6cd4a0a13f4b6e9791ecd8cb38e62e5760157516
Joseph <37001795+Amjos4web@users.noreply.github.com>
2018-06-30 02:21:31 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,54 @@
-let countriesCurrencies;
-		const dbPromise = idb.open('countries-currencies', 1, upgradeDB => {
-			switch (upgradeDB.oldVersion) {
-				case 0:
-				upgradeDB.createObjectStore('objs', {keyPath: 'id'});
-			}
+
+	// //Registering the Service Worker
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', () => {
+			navigator.serviceWorker.register('sw.js').then(registration => {
+				console.log('Service worker successfully registered on scope', registration.scope);
+			}).catch(error => {
+				console.log('Service worker failed to register');
+			});
 		});
+	}
+	
+	const dbPromise = idb.open('currency-rates', 1, upgradeDB => {
+		switch (upgradeDB.oldVersion) {
+			case 0:
+			upgradeDB.createObjectStore('objs', {keyPath: 'id'});
+		}
+	});
+
+	const currencyUrl = "https://free.currencyconverterapi.com/api/v5/currencies";
+	
+
+	fetch(currencyUrl)
+	.then(res => res.json())
+	.then(data => {
+		
+		for (const key in data) {
+		  return data[key];
+		}
+		//console.log(data);
+	})
+	.then(datakey => {
+		for (const key2 in datakey) {
+			const id = datakey[key2].id;
+			const curName = datakey[key2].currencyName;
+			console.log(id, curName);
+			$('#convertfrom, #convertto').append($('<option>').text(`${curName}  ${id}`).attr('value', id));
+		}
+	})
+	.catch(error => {
+		console.log(error);
+	})
+	
+	
+
+	const convertCurrency = () => 
+	{ 
+		//let countriesCurrencies;
 		
 		
-		dbPromise.then(db => {
-        if(!db) return;
-			return db.transaction('objs')
-			.objectStore('objs').getAll();
-		}).then(allObjs => {
+		
 		
 			const convertfrom = document.getElementById("convertfrom").value;
 			const convertto = document.getElementById("convertto").value;
@@ -24,19 +61,18 @@ let countriesCurrencies;
 				}).then(data => {
 					dbPromise.then(db => {
 						if(!db) return;
-						countriesCurrencies = [data.results];
 						const tx = db.transaction('objs', 'readwrite');
 						const store = tx.objectStore('objs');
-						countriesCurrencies.forEach(currency => {
-							for (let value in currency) {
-								store.put(currency[value]);
-							}
-						});
+						
+						//console.log(data);
+						//console.log(query);
+						store.put({id: query, rates: data});
 						return tx.complete;
 					});
+					
 					const oneUnit = data[query];
 					const amt = document.getElementById("fromAmount").value;
 					document.getElementById("amountConverted").value = (oneUnit*amt).toFixed(2);
 					
-				})
-		});
+				});
+	}
