1876dfea4f9126cbfbd34c7bd7a1aefffce51e82
Joseph <37001795+Amjos4web@users.noreply.github.com>
2018-06-30 11:31:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 	if ('serviceWorker' in navigator) {
 		window.addEventListener('load', () => {
 			navigator.serviceWorker.register('sw.js').then(registration => {
-				console.log('Service worker successfully registered on scope', registration.scope);
+				//console.log('Service worker successfully registered on scope', registration.scope);
 			}).catch(error => {
-				console.log('Service worker failed to register');
+				//console.log('Service worker failed to register');
 			});
 		});
 	}
@@ -33,46 +33,56 @@
 		for (const key2 in datakey) {
 			const id = datakey[key2].id;
 			const curName = datakey[key2].currencyName;
-			console.log(id, curName);
+			//console.log(id, curName);
 			$('#convertfrom, #convertto').append($('<option>').text(`${curName}  ${id}`).attr('value', id));
 		}
 	})
 	.catch(error => {
-		console.log(error);
+		//console.log(error);
 	})
 	
 	
 
-	const convertCurrency = () => 
+	const convertCurrency = (isRateFound) => 
 	{ 
-		//let countriesCurrencies;
 		
-		
-		
-		
-			const convertfrom = document.getElementById("convertfrom").value;
-			const convertto = document.getElementById("convertto").value;
-			const query = `${convertfrom}_${convertto}`;
-			const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+		const convertfrom = document.getElementById("convertfrom").value;
+		const convertto = document.getElementById("convertto").value;
+		const query = `${convertfrom}_${convertto}`;
+		const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
 
-			fetch(url)
-				.then(response => {
-					return response.json();
-				}).then(data => {
-					dbPromise.then(db => {
-						if(!db) return;
-						const tx = db.transaction('objs', 'readwrite');
-						const store = tx.objectStore('objs');
+		fetch(url)
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				dbPromise.then(db => {
+					if(!db) return;
+					const tx = db.transaction('objs', 'readwrite');
+					const store = tx.objectStore('objs');
+					
+					//console.log(data);
+					//console.log(query);
+					store.put({id: query, rates: data});
+					return tx.complete;
+				})
+				
+				
+				const oneUnit = data[query];
+				const amt = document.getElementById("fromAmount").value;
+				document.getElementById("amountConverted").value =  (oneUnit*amt).toFixed(2);
+				
+			})
+			.catch(() => {
+				if (!isRateFound) {
+					alert('Cannot convert this while offline');
+				}
 						
-						//console.log(data);
-						//console.log(query);
-						store.put({id: query, rates: data});
-						return tx.complete;
-					});
-					
-					const oneUnit = data[query];
-					const amt = document.getElementById("fromAmount").value;
-					document.getElementById("amountConverted").value = (oneUnit*amt).toFixed(2);
-					
-				});
+			});
+			
+			// if (amt.value === '') {
+				// alert('Please enter amount to convert');
+			// }
+		
 	}
+	
